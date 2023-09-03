d1f1e2300d50f53fa7f4c93d1a045ef4b91e2629
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2023-09-03 16:49:11 +0200

    Basic REPL stub

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::{Config, Editor};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config = Config::builder().auto_add_history(true).build();
+    let mut rl: Editor<(), rustyline::history::FileHistory> = Editor::with_config(config)?;
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(line) => {
+                if line == ".exit" {
+                    break;
+                }
+                println!("Line: {:?}", line);
+            }
+            Err(e) => match e {
+                rustyline::error::ReadlineError::Eof
+                | rustyline::error::ReadlineError::Interrupted => break,
+                _ => println!("Error: {:?}", e),
+            },
+        }
+    }
+    Ok(())
 }
