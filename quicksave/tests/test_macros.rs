76dcee2047242418c940d149b9c334b42b8dbd66
Aleksandr Kovalev <aleksandr@kovalev.engineer>
2023-11-28 12:36:27 +0100

    Add impl block generation for save_slot

--- quicksave/tests/test_macros.rs
+++ quicksave/tests/test_macros.rs
@@ -7,6 +7,10 @@ fn it_works() {
         name: String,
         age: u8,
     }
+
+    let person = Person::new("John".to_string(), 30);
+
+    person.save();
 }
 
 // todo: require to use a constructor method
--- quicksave_macros/src/lib.rs
+++ quicksave_macros/src/lib.rs
@@ -1,11 +1,12 @@
 extern crate proc_macro;
 
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as TokenStream1;
+use proc_macro2::TokenStream;
 use quote::quote;
-use syn::{parse, Fields, ItemStruct};
+use syn::{parse, Fields, FieldsNamed, ItemStruct};
 
 #[proc_macro_attribute]
-pub fn save_slot(_attr: TokenStream, item: TokenStream) -> TokenStream {
+pub fn save_slot(_attr: TokenStream1, item: TokenStream1) -> TokenStream1 {
     let strukt: ItemStruct = parse(item).unwrap();
 
     let fields = if let Fields::Named(fields) = &strukt.fields {
@@ -15,10 +16,56 @@ pub fn save_slot(_attr: TokenStream, item: TokenStream) -> TokenStream {
     };
 
     let name = strukt.ident;
-    let gen = quote! {
+    let generated_struct = quote! {
         struct #name
             #fields
     };
 
-    gen.into()
+    let args = named_fields_to_function_args(fields);
+    let names = named_fields_to_names(fields);
+
+    let impl_block = quote! {
+        impl #name {
+            fn new(#args) -> Self {
+                Self {
+                    #names
+                }
+            }
+
+            fn save(&self) {
+
+            }
+        }
+    };
+
+    let output = quote! {
+        #generated_struct
+        #impl_block
+    };
+    output.into()
+}
+
+fn named_fields_to_function_args(fields: &FieldsNamed) -> TokenStream {
+    let args = fields.named.iter().map(|field| {
+        let name = &field
+            .ident
+            .clone()
+            .expect("Only named fields are supported");
+        let ty = &field.ty;
+        quote! { #name: #ty }
+    });
+
+    quote! { #(#args),* }
+}
+
+fn named_fields_to_names(fields: &FieldsNamed) -> TokenStream {
+    let names = fields.named.iter().map(|field| {
+        let name = &field
+            .ident
+            .clone()
+            .expect("Only named fields are supported");
+        quote! { #name }
+    });
+
+    quote! { #(#names),* }
 }
